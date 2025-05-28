73ccfdad9ae4dfd7e0239c205bf0563e2b341c66
taiking0905 <taiki.noguchi0905@gmail.com>
2025-05-28 20:14:28 +0900

    test

--- scraper/scraper_slack.py
+++ scraper/scraper_slack.py
@@ -1,46 +1,38 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import os
+from slack_sdk import WebClient
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import os
-import discord
-import asyncio
 
-DISCORD_TOKEN = os.environ['DISCORD_BOT_TOKEN']
-CHANNEL_ID = int(os.environ['DISCORD_CHANNEL_ID'])  # 数字として扱う
 
-async def send_discord_message(message):
-    client = discord.Client(intents=discord.Intents.default())
-
-    @client.event
-    async def on_ready():
-        channel = client.get_channel(CHANNEL_ID)
-        await channel.send(message)
-        await client.close()
-
-    await client.start(DISCORD_TOKEN)
-
-def scrape_and_get_text():
+def scrape_and_notify():
     options = Options()
-    options.binary_location = "/usr/bin/chromium-browser"
+    options.binary_location = "/usr/bin/chromium-browser" 
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
 
     driver = webdriver.Chrome(options=options)
+    
     driver.get('https://fukaya-lab.azurewebsites.net/report.html?id=T122115')
 
+    # 最大10秒待機して要素が見つかるのを待つ
     wait = WebDriverWait(driver, 10)
     elem = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div[data-bind*="current().first"]')))
+
+    # innerTextが入るのをさらに待つ
     wait.until(lambda d: elem.text.strip() != '')
     text = elem.text.strip()
-    driver.quit()
-    return text
 
-if __name__ == "__main__":
-    text = scrape_and_get_text()
+    driver.quit()
+
     if not text:
-        asyncio.run(send_discord_message("まだだよ"))
+        client = WebClient(token=os.environ['SLACK_BOT_TOKEN'])
+        client.chat_postMessage(channel=os.environ['SLACK_CHANNEL'], text="まだだよ")
     else:
-        asyncio.run(send_discord_message(f"書いてあるね！\n{text}"))
+        client = WebClient(token=os.environ['SLACK_BOT_TOKEN'])
+        client.chat_postMessage(channel=os.environ['SLACK_CHANNEL'], text="書いてあるね！\n" + text)
+if __name__ == "__main__":
+    scrape_and_notify()
